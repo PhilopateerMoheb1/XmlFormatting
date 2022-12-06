b832bb0753683c5cbc271325a42113b3fc773911
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2022-12-06 23:53:39 +0200

    add changes to jsonconverter

--- JSON_Converter.java
+++ JSON_Converter.java
@@ -2,8 +2,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JSON_Converter {
-    private String JSON_text=new String();
 
+    private static String setText(XMLChecker xml,int index){
+        String temp=new String();
+        do{
+            temp+= xml.getCorrectXML().charAt(index);
+            index++;
+        }while(xml.getCorrectXML().charAt(index)!='<');
+        return temp;
+    }
+    private static ArrayList<String>CheckId(XMLChecker xml,int index){
+        ArrayList<String>arrayID=new ArrayList<>();
+        String s=new String();
+        while(xml.getCorrectXML().charAt(index)!='f'){//follower
+            s+=xml.getCorrectXML().charAt(index);
+            if(s.charAt((s.length()-1))=='>'){
+                arrayID.add(s);
+                arrayID.add(setText(xml,index));//to add the id
+                s="";
+            }
+        }
+        return arrayID;
+
+    }
    public static ArrayList<String> toArrayList(XMLChecker xml){
        ArrayList<String>arrayOfxml=new ArrayList<>();
        String s=new String();
@@ -31,6 +52,8 @@ public class JSON_Converter {
                       break;
                   case "<id>":
                       arrayOfxml.add(s);
+                      String temp =setText(xml,index);
+                      arrayOfxml.add(temp);
                       s = "";
                       break;
                   case "</id>":
@@ -39,6 +62,8 @@ public class JSON_Converter {
                       break;
                   case "<name>":
                       arrayOfxml.add(s);
+                      temp = setText(xml, index);
+                      arrayOfxml.add(temp);
                       s = "";
                       break;
                   case "</name>":
@@ -71,6 +96,8 @@ public class JSON_Converter {
                       break;
                   case "<body>":
                       arrayOfxml.add(s);
+                      temp =setText(xml,index);
+                      arrayOfxml.add(temp);
                       s = "";
                       break;
                   case "</body>":
@@ -79,6 +106,8 @@ public class JSON_Converter {
                       break;
                   case "<topic>":
                       arrayOfxml.add(s);
+                      temp =setText(xml,index);
+                      arrayOfxml.add(temp);
                       s = "";
                       break;
                   case "</topic>":
@@ -95,6 +124,10 @@ public class JSON_Converter {
                       break;
                   case "<follower>":
                       arrayOfxml.add(s);
+                      ArrayList<String>tempArray=CheckId(xml,index);
+                      for (int i=0;i<tempArray.size();i++){
+                          arrayOfxml.add(tempArray.get(i));
+                      }
                       s = "";
                       break;
                   case "</follower>":
@@ -116,35 +149,73 @@ public class JSON_Converter {
         }
         else{
          ArrayList<String>xmlarraylist=toArrayList(xml);
+            System.out.print("{\n");
             for (int i=0;i<xmlarraylist.size();i++){
-                System.out.print("{");
                 switch (xmlarraylist.get(i)) {
                     case "<users>":
                         System.out.print("\"users\": [\n");
                         break;
                     case "</users>":
-                        System.out.print("]");
+                        System.out.print("\n]");
                         break;
                     case "<user>":
                         System.out.print("\t\"user\":[ \n");
                         break;
                     case "</user>":
-                        System.out.print("\t]\n");
+                        System.out.print("\t]");
                         break;
                     case "<id>":
-                        System.out.print("\t\t\"id\":"+"\"\""+xmlarraylist.get(i+1)+"\"\",\n");
+                        System.out.print("\t\t\"id\":"+"\""+xmlarraylist.get(i+1)+"\",\n");
                         break;
                     case "<name>":
-                        System.out.print("\t\t\"name\":"+"\"\""+xmlarraylist.get(i+1)+"\"\",\n");
+                        System.out.print("\t\t\"name\":"+"\""+xmlarraylist.get(i+1)+"\",\n");
                         break;
                     case "<topics>":
-                        System.out.print("\t\t\t\"topics\":"+"\"\""+xmlarraylist.get(i+1)+"\"\",\n");
+                        System.out.print("\t\t\t\"topics\":[\n");
+                        break;
+                    case "</topics>":
+                        System.out.print("\t\t\t]\n");
+                        break;
+                    case "<topic>":
+                        System.out.print("\t\t\t\t\"topic\":\""+xmlarraylist.get(i+1)+"\"\n");
+                        break;
+                    case "<posts>":
+                        System.out.print("\t\t\"posts\":[ \n");
+                        break;
+                    case "</posts>":
+                        System.out.print("\t\t]\n");
+                        break;
+                    case "<post>":
+                        System.out.print("\t\t\t\"post\":[ \n");
+                        break;
+                    case "</post>":
+                        System.out.print("\t\t\t]\n");
+                        break;
+                    case "<body>":
+                        System.out.print("\t\t\t\t\"body\": ["+"\""+ xmlarraylist.get(i+1)+"\n");
+                        break;
+
+                    case "</body>":
+                        System.out.print("\t\t\t\t\n]");
+                        break;
+                    case "<followers>":
+                        System.out.print("\t\t \"followers\": [");
+                        break;
+                    case "<follower>":
+                        System.out.print("\t\t\t \"follower\":[");
+                        break;
+                    case "</follower>":
+                        System.out.print("\t\t\t]");
+                        break;
+
+                    case "</followers>":
+                        System.out.print("\t\t]");
                         break;
                     default:
                         break;
                 }
             }
-            System.out.print("}");
+            System.out.print("\n}");
 
         }
 
