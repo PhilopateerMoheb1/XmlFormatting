0a22b402b9e5f6a261cd4f8bbedd9443d790435a
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2022-12-11 20:43:18 +0200

    trying to make xml to json two level under construction

--- Tree.java
+++ Tree.java
@@ -32,39 +32,49 @@ public class Tree {
     }
 
     //O(n*n) //n element ,n childs we make recursion at each element worst case
-    public ArrayList<TreeNode> PutItInTree(int index,String[]Tags,String[]words,String name) {
+    public ArrayList<TreeNode> PutItInTree(int index,String[]Tags,String[]words,String name,String parent) {
         ArrayList<TreeNode>childerns=new ArrayList<>();
 
-
         for (int i = index; i < Tags.length - 1; i++) {
-            TreeNode node = new TreeNode();
             if (Tags[i] != "Visited") {
+                TreeNode node = new TreeNode();
                 String closingtagname = Tags[i].substring(2, Tags[i].length() - 1);
                 String nameTemp = name.substring(1, name.length() - 1);
-                if (!nameTemp.equals(closingtagname)) {
-                    String p = Tags[i].substring(0, 2);
-                    if (!p.equals("</")) {
-                        String temp = Tags[i].substring(1, Tags[i].length() - 1);
-                        String temp2 = Tags[i + 1].substring(2, Tags[i + 1].length() - 1);
-                        if (temp.equals(temp2)) {//<id></id>?id==id? to check equality of tags
-                            Tags[i] = "Visited";
-                            childerns.add(makeNode(temp, words[counter]));
-                            counter++;
-                            name = xmlChecker.getTags()[index - 1];
+                if (!parent.substring(1,parent.length()-1).equals(Tags[i].substring(2, Tags[i].length() - 1))||parent=="Root") {
+                    if (!nameTemp.equals(closingtagname)) {
+                        String p = Tags[i].substring(0, 2);
+                        if (!p.equals("</")) {
+                            String temp = Tags[i].substring(1, Tags[i].length() - 1);
+                            String temp2 = Tags[i + 1].substring(2, Tags[i + 1].length() - 1);
+                            if (temp.equals(temp2)) {//<id></id>?id==id? to check equality of tags
+                                Tags[i] = "Visited";
+                                childerns.add(makeNode(temp, words[counter]));
+                                counter++;
+                                name = xmlChecker.getTags()[index - 1];
+                                parent = xmlChecker.getTags()[index - 2];
+                            } else {
+                                node.setName(Tags[i]);
+                                Tags[i] = "Visited";
+                                childerns.add(node);
+                                node.setChilds(PutItInTree(i + 1, Tags, words, Tags[i + 1], Tags[i]));
+                                if (i > 0) {
+                                    parent = xmlChecker.getTags()[i - 1];
+                                }
+                                else{
+                                    parent=xmlChecker.getTags()[0];
+                                }
+                            }
+
                         } else {
-                            node.setName(Tags[i]);
                             Tags[i] = "Visited";
-                            childerns.add(node);
-                            node.setChilds(PutItInTree(i + 1, Tags, words, Tags[i + 1]));
                         }
 
                     } else {
                         Tags[i] = "Visited";
+                        return childerns;
                     }
-
                 }
                 else {
-                    Tags[i]="Visited";
                     return childerns;
                 }
             }
@@ -84,16 +94,29 @@ public class Tree {
         }
         root =new TreeNode();
         root.setName("Root");
-        root.setChilds(PutItInTree(0,Tags,words,Tags[0]));
+        root.setChilds(PutItInTree(0,Tags,words,Tags[0],"Root"));
     }
 
+    public boolean isNumeric(String s){
+        try {
+            Integer num = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
     public String printToJson(TreeNode node){
         ArrayList<TreeNode>nodes=node.getChilds();
         for (int i=0;i<nodes.size();i++){
             if(nodes.get(i).getChilds().size()==0){
-                Json_Text+=indent.peek()+"\""+nodes.get(i).getName()+"\": "+nodes.get(i).getData()+",";
+                if(isNumeric(nodes.get(i).getData())) {
+                    Json_Text += indent.peek() + "\"" + nodes.get(i).getName() + "\": " + nodes.get(i).getData();
+                }
+                else{
+                    Json_Text += indent.peek() + "\"" + nodes.get(i).getName() + "\": " + "\""+nodes.get(i).getData()+"\"";
+                }
                 if(nodes.size()>1&&i!=(nodes.size()-1)){
-                    Json_Text+="\n";
+                    Json_Text+=",\n";
                 }
             }
             else {
