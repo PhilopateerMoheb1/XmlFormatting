c484976fbb481e2018faa63c44dafd644f31c710
ahmedadelhassan159 <104680320+ahmedadelhassan159@users.noreply.github.com>
2022-12-11 14:49:26 +0200

    Update Formatting.java

--- Formatting.java
+++ Formatting.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package fro;
 
 import java.util.Stack;
 import java.util.Vector;
 
-/**
- *
- * @author  Ahmed Abd elmotelb
- */
+
 public class Formatting
 {
 	public static String format(String xmlchecking) {
