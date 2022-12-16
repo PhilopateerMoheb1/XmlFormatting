d96c244691fa66b8a899aa6b17332299a86be5a4
Philopateer Moheb <philopateermoheb1901202@gmail.com>
2022-12-16 23:28:47 +0200

    Feat: xml to json

--- Data Structure/Tree.java
+++ Data Structure/Tree.java
@@ -31,54 +31,44 @@ public class Tree {
 
     }
 
+
     //O(n*n) //n element ,n childs we make recursion at each element worst case
-    public ArrayList<TreeNode> PutItInTree(int index,String[]Tags,String[]words,String name,String parent) {
+    public ArrayList<TreeNode> PutItInTree(TreeNode node,int index) {
         ArrayList<TreeNode>childerns=new ArrayList<>();
-
-        for (int i = index; i < Tags.length - 1; i++) {
-            if (Tags[i] != "Visited") {
-                TreeNode node = new TreeNode();
+        for (int i=index;i<Tags.length;i++){
+            if (Tags[i] != "Visited"){
                 String closingtagname = Tags[i].substring(2, Tags[i].length() - 1);
-                String nameTemp = name.substring(1, name.length() - 1);
-                if (!parent.substring(1,parent.length()-1).equals(Tags[i].substring(2, Tags[i].length() - 1))||parent=="Root") {
-                    if (!nameTemp.equals(closingtagname)) {
-                        String p = Tags[i].substring(0, 2);
-                        if (!p.equals("</")) {
-                            String temp = Tags[i].substring(1, Tags[i].length() - 1);
-                            String temp2 = Tags[i + 1].substring(2, Tags[i + 1].length() - 1);
-                            if (temp.equals(temp2)) {//<id></id>?id==id? to check equality of tags
-                                Tags[i] = "Visited";
-                                childerns.add(makeNode(temp, words[counter]));
-                                counter++;
-                                name = xmlChecker.getTags()[index - 1];
-                                parent = xmlChecker.getTags()[index - 2];
-                            } else {
-                                node.setName(Tags[i]);
-                                Tags[i] = "Visited";
-                                childerns.add(node);
-                                node.setChilds(PutItInTree(i + 1, Tags, words, Tags[i + 1], Tags[i]));
-                                if (i > 0) {
-                                    parent = xmlChecker.getTags()[i - 1];
-                                }
-                                else{
-                                    parent=xmlChecker.getTags()[0];
-                                }
-                            }
+                String nameTemp = node.getName().substring(1, node.getName().length() - 1);
+                TreeNode n= new TreeNode();
+                String p = Tags[i].substring(0, 2);
+                if(!nameTemp.equals(closingtagname)){
+                    if (!p.equals("</")) {
+                        String temp = Tags[i].substring(1, Tags[i].length() - 1);
+                        String temp2 = Tags[i + 1].substring(2, Tags[i + 1].length() - 1);
 
-                        } else {
+                        if (temp.equals(temp2)) {//<id></id>?id==id? to check equality of tags
                             Tags[i] = "Visited";
+                            childerns.add(makeNode(temp, words[counter]));
+                            counter++;
+                        } else {
+                            n.setName(Tags[i]);
+                            Tags[i] = "Visited";
+                            n.setChilds(PutItInTree(n,index+1));
+                            childerns.add(n);
+                            node.setChilds(childerns);
                         }
-
-                    } else {
-                        Tags[i] = "Visited";
-                        return childerns;
+                    }
+                    else {
+                        Tags[i]="Visited";
                     }
                 }
-                else {
+                else{
+                    Tags[i]="Visited";
                     return childerns;
                 }
             }
-            }
+
+        }
         return childerns;
         }
 
@@ -93,8 +83,8 @@ public class Tree {
             }
         }
         root =new TreeNode();
-        root.setName("Root");
-        root.setChilds(PutItInTree(0,Tags,words,Tags[0],"Root"));
+        root.setName(Tags[2]);
+        root.setChilds(PutItInTree(root,3));
     }
 
     public boolean isNumeric(String s){
@@ -110,13 +100,6 @@ public class Tree {
         for (int i=0;i<nodes.size();i++){
             if(nodes.get(i).getChilds().size()==0){
                 if(isNumeric(nodes.get(i).getData())) {
-                    Json_Text += indent.peek() + "\"" + nodes.get(i).getName() + "\": " + nodes.get(i).getData();
-                }
-                else{
-                    Json_Text += indent.peek() + "\"" + nodes.get(i).getName() + "\": " + "\""+nodes.get(i).getData()+"\"";
-                }
-                if(nodes.size()>1&&i!=(nodes.size()-1)){
-                    Json_Text+=",\n";
                     Json_Text.append(indent.peek() + "\"" + nodes.get(i).getName() + "\": " + nodes.get(i).getData());
                 }
                 else{
@@ -130,6 +113,7 @@ public class Tree {
                 if(indent.isEmpty()){
                     Json_Text.append("{\n");
                     indent.push("    ");
+
                 }
                 Json_Text.append(indent.peek()+"\""+nodes.get(i).getName().substring(1,nodes.get(i).getName().length()-1)+"\": [\n");
                 String indention=indent.peek()+"    ";
@@ -142,10 +126,12 @@ public class Tree {
         Json_Text.append("\n"+indent.peek()+"]\n");
         return Json_Text;
     }
-    public void Print(){
-        printToJson(root.getChilds().get(0));
+    public StringBuilder Print(){
+
+        printToJson(root);
         Json_Text.append("\n}\n");
-        System.out.print(Json_Text);
+        return Json_Text;
+
     }
 
     boolean isEmpty(){
